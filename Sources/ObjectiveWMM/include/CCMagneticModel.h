2e210f69d766c77f9e8e2074968e94a47d69c15d
pryder-fleetaero <peter.ryder@fleetaeronautics.com.au>
2022-05-13 15:42:49 +1000

    Move header location for SPM build

--- Sources/ObjectiveWMM/include/CCMagneticModel.h
+++ Sources/ObjectiveWMM/include/CCMagneticModel.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-#import "CCMagneticDeclination.h"
+#import <CCMagneticDeclination.h>
 
 @interface CCMagneticModel : NSObject
 
